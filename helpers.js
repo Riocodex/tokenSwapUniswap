ed41a5ba9a78bae6db8eadcc9ea705a7143befd5
Rio <onwukachibike@gmail.com>
2023-03-09 21:20:57 +0100

    added weth and uniswap token info

--- helpers.js
+++ helpers.js
@@ -8,3 +8,17 @@ require('dotenv').config()
 const INFURA_URL_TESTNET = process.env.INFURA_URL_TESTNET
 const WALLET_ADDRESS = process.env.WALLET_ADDRESS
 const WALLET_SECRET = process.env.WALLET_SECRET
+
+const provider = new ethers.providers.JsonRpcProvider(INFURA_URL_TESTNET)//goerli
+const poolAddress ="0x4D7C363DED4B3b4e1F954494d2Bc3955e49699cC"
+const swapRouterAddress = '0xE592427A0AEce92De3Edee1F18E0157C05861564'
+
+const name0 = 'Wrapped Ether'
+const symbol0 = 'WETH'
+const decimals0 = 18
+const address0 = '0xc778417e063141139fce010982780140aa0cd5ab'
+
+const name1 = 'Uniswap Token'
+const symbol1 = 'UNI'
+const decimals1 = 18
+const address1 = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
