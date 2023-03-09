f94f11af11a5a722396350f8031cfe613077106c
Rio <onwukachibike@gmail.com>
2023-03-09 21:40:04 +0100

    updated

--- helpers.js
+++ helpers.js
@@ -1,5 +1,7 @@
 exports.getPoolImmutables = async(poolContract) =>{
-    const [factory, token0, token1, fee] = await Promise.all([
-        
+    const [token0, token1, fee] = await Promise.all([
+        poolContract.token0,
+        poolContract.token1,
+        poolContract.fee
     ])
 }
