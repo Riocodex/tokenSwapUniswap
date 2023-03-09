c4ac078e093c90739ebd270e73a986cdefb0025a
Rio <onwukachibike@gmail.com>
2023-03-09 21:15:35 +0100

    updated env variables

--- helpers.js
+++ helpers.js
@@ -3,3 +3,8 @@ const { abi: IUniswapV3PoolABI } = require('@uniswap/v3-core/artifacts/contracts
 const { abi: SwapRouterABI} = require('@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json')
 const { getPoolImmutables, getPoolState } = require('./helpers')
 const ERC20ABI = require('./abi.json')
+
+require('dotenv').config()
+const INFURA_URL_TESTNET = process.env.INFURA_URL_TESTNET
+const WALLET_ADDRESS = process.env.WALLET_ADDRESS
+const WALLET_SECRET = process.env.WALLET_SECRET
