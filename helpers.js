1a1812c900241ace4c4b9efd26a28a18fc0259dc
Rio <onwukachibike@gmail.com>
2023-03-09 22:05:34 +0100

    updated

--- helpers.js
+++ helpers.js
@@ -1,23 +1,24 @@
-exports.getPoolImmutables = async(poolContract) =>{
+exports.getPoolImmutables = async (poolContract) => {
     const [token0, token1, fee] = await Promise.all([
-        poolContract.token0,
-        poolContract.token1,
-        poolContract.fee
+      poolContract.token0(),
+      poolContract.token1(),
+      poolContract.fee()
     ])
-
+  
     const immutables = {
-        token0:token0,
-        token1:token1,
-        fee:fee
+      token0: token0,
+      token1: token1,
+      fee: fee
     }
     return immutables
-}
-
-exports.getPoolState = async (poolContract) =>{
+  }
+  
+  exports.getPoolState = async (poolContract) => {
     const slot = poolContract.slot0()
-
+  
     const state = {
-        sqrtPriceX96: slot[0]
+      sqrtPriceX96: slot[0]
     }
+  
     return state
-}
+  }
